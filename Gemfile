765e1e82d8b44529303c5dea99f551d57afc9b91
Hector Leon <hectorleon.designer@gmail.com>
2015-02-21 19:43:49 +0100

    lightbox fixed

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'uglifier', '>= 1.3.0'
 
 #Google maps gem
 gem 'gmaps4rails'
+#Underscore (gmaps4rails depends of this)
+gem 'underscore-rails'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,31 +1,55 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-//jQuery
-$(document).ready(function(){
-
-
-	// Button Burguer animation
-	$("#js-btn--burguer").click(function() {
-	  $(this).toggleClass("js-active");
-	  toggleMenu();
-	  toggleShadow();
+$(document).on('click', '#js-btn--burguer', function() {
+  
+  $(this).toggleClass("js-active");
+  var transitionTime = 700;
+    toggleMenu(transitionTime);
+    toggleShadow(transitionTime);
     toggleBlockScroll();
-	});
 
-function toggleMenu (){
-	$("#js-nav--toggle").toggleClass("js-show-menu");
-}
+  $(".js-over-lightbox").on('click', 'a, button, input[type=submit]', function(e){
+    //add here animation to clicked element
+    //use e.offsetX y e.offsetY to center the animation in the point clicked
+    console.log(e);
+  });
 
-function toggleShadow(){
-    $("#js-lightbox").toggleClass("js-lightbox--active");
-}
-function toggleBlockScroll(){
+  function toggleMenu(transitionTime){
+    $("#js-menu--toggle").css('transition', transitionTime+'ms')
+                         .toggleClass("js-menu--toggle");
+  };
+
+  function toggleBlockScroll(){
     $("body").toggleClass("js-block-scroll");
-}
+  }
 
 
+  /* -----------------------------
+  /*                    LIGHTBOX
+  /* -----------------------------
+  /*
+  /* The lightbox is used to put a shadow that puts in background some content. This feature has
+  /* the option to add a class to the parent element to put over the lightbox and stay in foreground.
+  /* Example to Toggle Lightbox Effect: <button onclick="toggleShadow()">Click me </button>
+  /* Example element over lightbox: <nav class="js-over-lightbox">...</nav>
+  /*
+  /* ----------------------------- */
 
+  function toggleShadow(transitionTime){
+    $("#js-lightbox").length ? removeLightbox(transitionTime) : createLightbox(transitionTime);
+  }
 
+  function createLightbox(transitionTime){
+    $(".js-over-lightbox").addClass("js-over-lightbox--active");
+    $("body").append('<div id="js-lightbox" class="js-lightbox"></div>');
+    $("#js-lightbox").fadeTo(transitionTime, .4).addClass("js-lightbox--active");
+  }
+
+  function removeLightbox(transitionTime){
+    $("#js-lightbox").fadeTo(transitionTime,0);
+    setTimeout(function(){ 
+      $(".js-over-lightbox").removeClass("js-over-lightbox--active");
+      $("#js-lightbox").remove()
+    }, transitionTime)
+  }
 
 
 
