dbbeac106cd55b44c81128d415f6501bf5c347ef
Hector Leon <hectorleon.designer@gmail.com>
2015-03-03 10:23:26 +0100

    More styling, adding pic and reduce links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
   scope "(:locale)" do
-    get '/aboutus'   => 'site#aboutus'
-    get '/contact'   => 'site#contact'
+    get '/aboutus',   to: 'site#aboutus'
+    get '/contact',   to: 'site#contact'
+    get '/services',  to: 'site#services'
     # EL root tiene que ser el último porque sino locale me cogerá la url del index
     # y la liará parda
     resources :routes do
